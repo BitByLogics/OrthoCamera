c01d9648ce1a16330b715faf14b91fcaa7caa775
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-09-19 17:48:59 -0400

    Add option to hide world border

---
+++ src/main/java/com/dimaskama/orthocamera/mixin/WorldRendererMixin.java
@@ -0,0 +1,29 @@
+package com.dimaskama.orthocamera.mixin;
+
+import com.dimaskama.orthocamera.client.OrthoCamera;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+
+    @Inject(
+            method = "renderWorldBorder",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void hideWorldBorder(Camera camera, CallbackInfo ci) {
+        if (!OrthoCamera.isEnabled() || !OrthoCamera.CONFIG.hide_world_border) {
+            return;
+        }
+
+        ci.cancel();
+    }
+
+}
+
